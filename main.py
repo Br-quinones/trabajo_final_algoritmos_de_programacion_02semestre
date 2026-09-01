7afeee8b558459e0f62b72fb6e6ed094f174f02f
Br-Quiones <br-quinones@protonmail.com>
2026-08-31 21:41:38 -0500

    Mi primer commit: Aprendiendo lectura de CSV

---
+++ main.py
@@ -0,0 +1,31 @@
+from csv import reader
+from statistics import mode
+import csv
+import pathlib
+import time
+import rich.console
+
+clean_console = rich.console.Console().clear
+
+####
+matrix_base = [[1,2,3],
+               [4,5,6],
+               [7,8,10],]
+
+###
+date_base = pathlib.Path("one.csv")
+with open(date_base, mode="w", newline="", encoding="utf-8") as f:
+     writer = csv.writer(f)
+     writer.writerows(matrix_base)
+
+###
+with open(date_base, mode="r", encoding="utf-8") as f:
+     read = csv.reader(f)
+     convert_matrix = list(read)
+
+###
+for row in convert_matrix:
+     print(row)
+
+
+     
