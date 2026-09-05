98cb2d6f8898a67355ccc6096d1e333aa413facc
Br-Quiones <br-quinones@protonmail.com>
2026-09-04 23:00:56 -0500

    Agregar parte visual con Rich del proyecto

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import menu
 from the_matrix import MyTable
+import menu
 
 main_table = MyTable(8,2)
-while True:
-    main_table.see()
 
+while True:
+    menu.main_menu(main_table)
+    
     what_add = input("Ingrese a agregar (r/c): ")
 
     if what_add == "r":
--- menu.py
+++ menu.py
@@ -1,4 +1,43 @@
 from rich.console import Console
+from rich.table import Table
 
 def clean():
     Console().clear()
+
+def line():
+    Console().rule(style="White")
+
+def heat_menu():
+    heat_menu_table = Table(show_header=False, expand=True)
+
+    heat_menu_table.add_column(justify="left",   ratio=1)
+    heat_menu_table.add_column(justify="center", ratio=1)
+    heat_menu_table.add_column(justify="right",  ratio=1)
+
+    heat_menu_table.add_row("F1: Load CSV", "F2: Save CSV", "f3: Exit")
+
+    Console().print(heat_menu_table)
+    
+def commands_menu():
+    commands_menu_rich = Table(show_header=False, expand=True)
+
+    commands_menu_rich.add_column(justify="left",   ratio=1)
+    commands_menu_rich.add_column(justify="center", ratio=1)
+    commands_menu_rich.add_column(justify="right",  ratio=1)
+
+    commands_menu_rich.add_row("Use 'arrows' to move", "Use 'control + arrows' to add or remove column or row", "coming soon")
+
+    Console().print(commands_menu_rich)
+
+
+def main_menu(matrix_to_print):
+    line()
+    heat_menu()
+    line()
+    matrix_to_print.see()
+    line()
+    commands_menu()
+    line()
+
+
+
